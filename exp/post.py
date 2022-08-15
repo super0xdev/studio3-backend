6dfa1c7d3bbda2e84b5f2bb476bbf2e300fa6185
myware <alphaprime8888@gmail.com>
2022-08-15 21:31:40 +0800

    added stuff

--- exp/post.py
+++ exp/post.py
@@ -3,6 +3,13 @@ import requests
 import base58
 import time
 
+PRODUCTION_MODE = True
+
+if PRODUCTION_MODE:
+    api_url = 'https://j0624ut64a.execute-api.us-east-1.amazonaws.com/login'
+else:
+    api_url = 'http://localhost:5000/login'
+
 # sign
 keypair = Keypair.generate()
 timestamp = int(time.time())
@@ -12,12 +19,17 @@ signed = keypair.sign(msg)
 signatureb58 = base58.b58encode(bytes(signed))
 signatureb58_decoded = signatureb58.decode()
 
-# pack
+# test register
 data = {'address': pubkey, 'timestamp':timestamp, 'signature':signatureb58_decoded}
-
-# send
-api_url = 'http://localhost:5000/login'
 r = requests.post(url=api_url, json=data)
 print(r.status_code, r.reason, r.text)
 
+# test login
+data = {'address': pubkey, 'timestamp':timestamp, 'signature':signatureb58_decoded}
+r = requests.post(url=api_url, json=data)
+print(r.status_code, r.reason, r.text)
 
+# test signature
+data = {'address': pubkey, 'timestamp':0, 'signature':signatureb58_decoded}
+r = requests.post(url=api_url, json=data)
+print(r.status_code, r.reason, r.text)
