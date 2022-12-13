235f454813fa90c7fe3a9fc6ca23f6a33e058a70
alphaprime8 <alphaprime8888@gmail.com>
2022-12-13 20:32:19 +0800

    updated base url for s3

--- conf/consts.py
+++ conf/consts.py
@@ -3,7 +3,9 @@ from dotenv import load_dotenv
 import os
 load_dotenv()
 
-S3_BASE_URL = "https://d3-studio-assets-1660843754077.s3.us-west-1.amazonaws.com/"
+# S3_BASE_URL = "https://d3-studio-assets-1660843754077.s3.us-west-1.amazonaws.com/"
+S3_BASE_URL = "https://d3-studio-assets.s3.us-east-2.amazonaws.com/"
+
 JWT_SECRET_KEY = os.environ.get("jwt_secret_key")
 MAX_FILE_SIZE_BYTES = int(10e6)
 
