f8d4e395ae3b0baacad835d4c413d9a2534b669d
alphaprime8 <alphaprime8888@gmail.com>
2022-12-15 18:03:05 +0800

    watermark dynamic sizing works

--- image_utils/add_watermark.py
+++ image_utils/add_watermark.py
@@ -3,12 +3,31 @@ from PIL import Image
 
 
 def add_watermark(asset_fpath):
+
+    # TODO make dynamic
+
     watermark_fpath = "./image_utils/studio3_watermark.png"
 
     asset_image = Image.open(asset_fpath)
+
+    asset_height = asset_image.height
+    asset_width = asset_image.width
+    print(f"got asset heigh widhthj: {asset_height}, {asset_width}")
+
     watermark_image = Image.open(watermark_fpath)
 
-    watermark_image.thumbnail((200, 200))
+    mark_height = watermark_image.height
+    mark_width = watermark_image.width
+    print(f"mark height, width, {mark_height}, {mark_width}")
+
+    mark_ar = mark_width / mark_width
+
+    target_height = int(asset_height/4)
+    target_width = int(target_height * mark_ar)
+
+    # TOD rever height and width?
+    watermark_image.thumbnail((target_width, target_height))
+
     copied_image = asset_image.copy()
     copied_image.paste(watermark_image, (50, 50))
 
