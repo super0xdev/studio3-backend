96d60304a7d828513ae8e58658651eeca63e8225
alphaprime8 <alphaprime8888@gmail.com>
2022-12-17 02:24:17 +0800

    fixed mask transparecnty

--- image_utils/add_watermark.py
+++ image_utils/add_watermark.py
@@ -43,7 +43,7 @@ def add_watermark(asset_fpath):
 
     copied_image = asset_image.copy()
     # copied_image.paste(watermark_image, (16, 16))
-    copied_image.paste(watermark_image, (asset_width - 15 - target_width, asset_height - target_height - 16))
+    copied_image.paste(watermark_image, (asset_width - 15 - target_width, asset_height - target_height - 16), watermark_image)
 
     # TODO maybe save sa jepg for alpha...
     final_image = copied_image.convert('RGB')
