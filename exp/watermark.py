4c1df713f683070e0797005acf70aafca33b1d04
alphaprime8 <alphaprime8888@gmail.com>
2022-12-17 02:23:43 +0800

    fixed

--- exp/watermark.py
+++ exp/watermark.py
@@ -4,18 +4,26 @@ import matplotlib.pyplot as plt
 import numpy as np
 
 # load
-asset_fpath = "./tmp_upload/droid_pfp.png"
-watermark_fpath = "./tmp_upload/watermark.png"
+asset_fpath = "./tmp_upload/droid_alpha.png"
+watermark_fpath = "./image_utils/s2_watermakrk_1.png"
 
 asset_image = Image.open(asset_fpath)
 watermark_image = Image.open(watermark_fpath)
 
-# asset_image.show()
-# plt.imshow(asset_image)
+mark_ar = watermark_image.height / watermark_image.width
 
-watermark_image.thumbnail((200, 200))
+target_width = int(asset_image.width / 3)
+target_height = int(target_width * mark_ar)
+
+print(f"render watermakrk")
+
+# TOD rever height and width?
+print(f"watermarker {target_width}, {target_height}")
+
+watermark_image.thumbnail((target_width, target_height))
+
 copied_image = asset_image.copy()
-copied_image.paste(watermark_image, (50, 50))
+copied_image.paste(watermark_image, (50, 50), watermark_image)
 
 plt.imshow(copied_image)
 plt.show()
